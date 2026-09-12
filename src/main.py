814d682e56121c0abcc9e4894bfd7ca135973eaf
René Núñez <221588492+rene-nunez@users.noreply.github.com>
2026-09-12 10:43:16 -0600

    chore: change LICENSE to GPLv3

--- src/main.py
+++ src/main.py
@@ -1,7 +1,19 @@
 """
-Clasificador de Spam
+    clasificador-spam: Modelos para clasificar mensajes de spam en español
+    Copyright (C) 2026 René Núñez
 
-Ejecuta todo el proceso desde la exploracion hasta la prediccion (pipeline completo).
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <https://www.gnu.org/licenses/>.
 """
 
 import os
